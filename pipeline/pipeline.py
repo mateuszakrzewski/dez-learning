bcc60382efb022d6783bf9101a9c25f23deedd02
Rejtanek <mateuszz.zzakrzewski@gmail.com>
2026-09-06 22:58:50 +0200

    containerization

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,1 +1,12 @@
-import pandas
+import sys
+print("arguments", sys.argv)
+
+day = int(sys.argv[1])
+print(f"running pipelines for day {day}")
+
+import pandas as pd
+
+df = pd.DataFrame({"A":[1,2], "B":[3,4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
